5f7dabcfd22f7fe0ca32336f702446d389c0d892
andyliang <lwm634@163.com>
2017-12-04 14:40:43 +0800

    新增文件

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+/* 首页. */
+router.get('/index', function(req, res, next) {
+  try {
+    res.render('index',
+        {
+          title: '首页'
+        }
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/instru', function(req, res, next) {
+    try {
+        res.render('productInstru', {
+            title: '产品介绍',
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/list', function(req, res, next) {
+    try {
+        res.render('productlist', {
+            title: '产品列表',
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/detail', function(req, res, next) {
+    try {
+        res.render('productdetail', {
+            title: '产品详情',
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/about', function(req, res, next) {
+    try {
+        res.render('about', {
+            title: '关于我们',
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/service', function(req, res, next) {
+    try {
+        res.render('service', {
+            title: '服务介绍',
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var webpack = require('webpack');
+var glob = require('glob')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+//路径是相对于package.json所在路径
+var entry_map = {
+    'index': './public/javascripts/index.js',
+};
+var pages = {};
+glob.sync('./views/**/*.html').forEach(function (entry) {
+    var basename = path.basename(entry, path.extname(entry));
+    pages[basename] = entry;
+    console.log(basename, entry, '=============');
+});
+
+var webpackConfig = {
+    entry: entry_map,
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(process.cwd(), 'dist/'),
+        //[name]-[hash].js可以指定hash值。
+        filename: '[name][hash].js',
+    },
+    plugins: [
+        // new ExtractTextPlugin("[name][hash].css"),
+        new ExtractTextPlugin({
+            filename: path.posix.join('static', 'css/[name]-extract.[contenthash].css')
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /(node_modules)|(global\/lib\/)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader','css-loader')
+            },
+            {
+                test: /\.scss$/,
+                //!代表先执行 ？是传递给loader的参数，?sourceMap表示使用sourceMap， js使用sourcemap通过devtool: sourcemap来指定。
+                loader: ExtractTextPlugin.extract('style-loader','css-loader?sourceMap&-convertValues!sass-loader?sourceMap')
+            },
+            { //解析 .html
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ]
+    }
+};
+
+for (var pathname in pages) {
+    console.log(pathname, '--------');
+    var conf = {
+        filename: pathname + '.html',
+        template: pages[pathname],
+        inject: true,
+        chunks: [pathname],
+    };
+    if (pathname in webpackConfig.entry) {
+        conf.chunks = ['vendor', pathname];
+        conf.hash = true;
+    }
+    webpackConfig.plugins.push(new HtmlWebpackPlugin(conf));
+}
+
+module.exports = webpackConfig;
